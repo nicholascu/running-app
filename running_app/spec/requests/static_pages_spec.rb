cff010600d844a27b0f1c0f88ad80c589eb00af7
nicholascu <nicholascu@gmail.com>
2014-07-05 23:32:09 -0700

    Add title to pages with helper method for full vs base title

--- running_app/spec/requests/static_pages_spec.rb
+++ running_app/spec/requests/static_pages_spec.rb
@@ -1,11 +1,45 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
 	describe "Home page" do
-		it "should have the content 'Sample App'" do
+
+		it "should have the content'Sample App'" do
 			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
+			expect(page).to have_content('Running App')
 
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Running App | Home")
+		end
+
 	end	
+
+	describe "Help Page" do
+		it "should have the content 'Help'" do
+			visit "/static_pages/help"
+			expect(page).to have_content("Help")
+		end
+
+		it "should have the right title" do 
+			visit '/static_pages/help'
+			expect(page).to have_title("Help")
+		end
+	end
+
+	describe "About Page" do
+		it "should have the content 'About Me'" do
+			visit "/static_pages/about"
+			expect(page).to have_content("About Me")
+		end
+
+		it "should have the right title" do 
+			visit '/static_pages/about'
+			expect(page).to have_title("About")
+		end
+	end
+
+
 end
