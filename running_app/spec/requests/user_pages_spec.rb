7662f9c186e2c05c5a912f82082ec1f61b59982d
nicholascu <nicholascu@gmail.com>
2014-07-06 19:49:46 -0700

    create new route for users new, signup

--- running_app/spec/requests/user_pages_spec.rb
+++ running_app/spec/requests/user_pages_spec.rb
@@ -1,21 +1,13 @@
 require 'spec_helper'
 
-describe "UserPages" do
-  # describe "GET /user_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get user_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
-  describe "User pages" do
-  	subject{page}
+describe "User pages" do
 
-  	describe "signup page" do
-  	end
-  	before {visit signup_path}
+  subject { page }
 
-  	it {should have_content("Sign Up")}
-  	it {should have_title(full_title('Sign Up'))}
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 end
